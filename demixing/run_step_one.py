997a1c47017e2a1c800a1ad898ee9e677abe2b75
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2021-10-18 15:36:09 +0100

    add run step one script

---
+++ demixing/run_step_one.py
@@ -0,0 +1,19 @@
+import pickle
+from dataset import StepOneDataset
+from os.path import dirname
+from pathlib import Path
+from read import parse_config
+MODULE_PATH = Path(dirname(__file__))
+CONFIG = parse_config()
+
+
+def main():
+    trn_seg = CONFIG['train_segments']
+    tst_seg = CONFIG['test_segments']
+    path = f'./data/trn{trn_seg}_tst{tst_seg}.pkl'
+    train_ds = StepOneDataset(path, CONFIG, state='train')
+    print(train_ds[:5])
+
+
+if __name__ == '__main__':
+    main()
